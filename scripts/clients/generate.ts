e63d17986355ce6ebc8233c2af9b344531064288
John Royal <34844819+john-royal@users.noreply.github.com>
2025-12-13 04:22:57 -0500

    chore(scripts): sdk generator incorrectly formats output files (#1260)

--- scripts/clients/generate.ts
+++ scripts/clients/generate.ts
@@ -28,7 +28,11 @@ export const generate = async () => {
   await $`rm -rf src/util/api`;
   await $`mkdir -p src/util/api`;
   await $`mv src/${clients[0]}/api/client/ src/${clients[0]}/api/core/ src/util/api/`;
-  await $`bunx oxlint src/util/api`;
+  const clientEntry = Bun.file(`alchemy/src/util/api/client/index.ts`);
+  await clientEntry.write(
+    (await clientEntry.text()).replaceAll(/(.*)\.gen/g, "$1.gen.ts"),
+  );
+  await $`bun oxfmt src/util/api`;
 
   // 3. Remove unused code
   for (const client of clients.slice(1)) {
@@ -40,7 +44,7 @@ export const generate = async () => {
   // 4. Update imports
   for (const client of clients) {
     await patchClientImports(client);
-    await $`bunx oxlint src/${client}/api`;
+    await $`bun oxfmt src/${client}/api`;
   }
 };
 
@@ -48,7 +52,11 @@ const patchClientImports = async (client: string) => {
   for (const name of ["client.gen.ts", "sdk.gen.ts"]) {
     const file = Bun.file(`alchemy/src/${client}/api/${name}`);
     const content = await file.text();
-    await file.write(content.replace("./client", "../../util/api/client"));
+    await file.write(
+      content
+        .replace("./client", "../../util/api/client/index.ts")
+        .replaceAll(/(.*)\.gen/g, "$1.gen.ts"),
+    );
   }
 };
 
